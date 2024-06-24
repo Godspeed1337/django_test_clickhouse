f6396951d1479da79fb84aacd22ec54b8c5b1651
Godspeed <maddust2002@gmail.com>
2024-06-24 03:54:30 +0300

    test_commit_again

--- clickhouse/urls.py
+++ clickhouse/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add', views.add, name='add'),
+    path('add_4', views.add, name='add'),
     path('avg', views.average, name='avg'),
     path('graph', views.graph, name='graph'),
 ]
